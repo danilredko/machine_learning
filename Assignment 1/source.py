99fe30c77f9849e27a5f81a96631d31907aef57a
Danil Redko <user@utmuser147-241.wireless.utoronto.ca>
2018-10-01 11:33:07 -0400

    first commit

---
+++ Assignment 1/source.py
@@ -0,0 +1,17 @@
+import numpy as np
+import numpy.random as rnd
+
+print ("\n\nQuestion 1")
+print ("-----------------------------------------------------")
+
+print ("\nQuestion 1(a): ")
+A = rnd.rand(4,3)
+print(A)
+print ("-----------------------------------------------------")
+
+print ("\nQuestion 1(b): ")
+x = rnd.rand(4,1)
+print(x)
+print ("-----------------------------------------------------")
+
+print ("\nQuestion 1(c): ")
