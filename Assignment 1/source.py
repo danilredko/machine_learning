1a50dbea26ebfa3b876355922b564836790192c4
Danil Redko <user@utmuser147-241.wireless.utoronto.ca>
2018-10-04 15:53:50 -0400

    Question 2 done

--- Assignment 1/source.py
+++ Assignment 1/source.py
@@ -1,6 +1,11 @@
 import numpy as np
 import numpy.random as rnd
+import time
+from numpy import linalg as la
 
+#Question 1
+
+"""
 print ("\n\nQuestion 1")
 print ("-----------------------------------------------------")
 
@@ -67,3 +72,51 @@ print ("-----------------------------------------------------")
 print ("\nQuestion 1(n): ")
 print(np.dot(np.transpose(A), x))
 print ("-----------------------------------------------------")
+
+"""
+
+#Question 2
+print("-----------------------------------------------------------------------")
+print("Question 2 a)")
+
+
+def cube_helper(A, B):
+
+    answer = np.zeros(A.shape)
+
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(A)):
+                answer[i][j] += A[i][k] * B[k][j]
+    return answer
+
+
+def cube(A):
+
+    return cube_helper(cube_helper(A,A),A)
+
+print("-----------------------------------------------------------------------")
+print("Question 2 b)")
+
+def mymeasure(N):
+
+
+    A = rnd.rand(N,N)
+    start_for_cube1 = time.time()
+    Cube1 = np.dot(np.dot(A,A), A)
+    print(str(time.time()-start_for_cube1)+' seconds for Cube1 using numpy')
+
+    start_for_cube2 = time.time()
+    Cube2 = cube(A)
+    print(str(time.time()-start_for_cube2)+' seconds for Cube2 using loops')
+
+    print('Magnitude of the difference is'+str(la.norm(np.subtract(Cube1, Cube2), np.inf)))
+
+
+print("-----------------------------------------------------------------------")
+print("Question 2 c) N=200")
+mymeasure(200)
+print("-----------------------------------------------------------------------")
+print("Question 2 c) N=2000")
+mymeasure(2000)
+print("-----------------------------------------------------------------------")
