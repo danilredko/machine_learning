9228dcdfc2eca0fcd8d6be839fb7514b9a0aa12b
Danil Redko <user@utmuser147-241.wireless.utoronto.ca>
2018-10-31 22:58:19 -0400

    q2 progress

--- Assignment2/source.py
+++ Assignment2/source.py
@@ -1,6 +1,9 @@
 import numpy as np
 import sklearn as sklrn
+import sklearn.linear_model as lmodel
 import matplotlib.pyplot as plt
+
+
 
 print("Question 1")
 print('')
@@ -44,5 +47,65 @@ def q1c(X, t):
 
     return 0
 
-q1c(X, t)
+#q1c(X, t)
+
+print("Question 2")
+print('')
+
+# Question 2 a)
+
+mu0 = np.array([0, -1])
+Sigma0 = np.array([[2.0, 0.5], [0.5, 1.0]])
+mu1 = np.array([-1, 1])
+Sigma1 = np.array([[1.0, -1.0], [-1.0, 2.0]])
+X, t = genData(mu0, mu1, Sigma0, Sigma1, 1000)
+
+# Question 2 b)
+
+logisticReg = lmodel.LogisticRegression()
+
+logisticReg.fit(X, t)
+
+w_0 = logisticReg.intercept_
+
+weights = logisticReg.coef_[0]
+
+mean_accuracy = logisticReg.score(X, t)
+
+
+print("w_0 : {}". format(w_0))
+print("")
+print("weight vector: {}".format(weights))
+print("")
+print("Accuracy: {}".format(mean_accuracy))
+
+print("")
+print("Qestion 2 c)")
+print("")
+
+def q2c(X, t):
+
+    plt.scatter(X[:, [0]], X[:, [1]], s=2, c=np.where(t == 0.0, 'r', 'b'))
+    x = np.linspace(-5, 5, 50)
+    plt.plot(x, np.add(np.dot(weights[1].T, x), w_0), 'black')
+    plt.suptitle("Question 2(c): training data and decision boundary")
+    plt.show()
+
+#q2c(X, t)
+
+# Question 2 d)
+
+print(" Question 2 d)")
+
+def q2d(X, t):
+
+    thres = np.arange(-3, 4)
+
+    print(thres)
+
+q2d(X, t)
+
+
+
+
 
